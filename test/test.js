576fc789d156db5ffe1c65cf19dba29802cdedd9
sftl <frantrinhluong@gmail.com>
2017-03-13 20:09:03 +0100

    M test.js: added new describe for 'Events API'

--- test/test.js
+++ test/test.js
@@ -1,8 +1,32 @@
-var assert = require('assert');
+var assert   = require('assert');
+var chai     = require('chai');
+var chaiHttp = require('chai-http');
+let should   = chai.should();
+
+chai.use(chaiHttp);
+
 describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
+	describe('#indexOf()', function() {
+		it('should return -1 when the value is not present', function() {
+			assert.equal(-1, [1,2,3].indexOf(4));
+		});
+	});
+});
+
+/*
+* Test the /GET route
+*/
+describe('/GET events', () => {
+	it('It should GET all the events', (done) => {
+		chai.request('http://localhost:3000')
+		.get('/events')
+		.end((err, res) => {
+
+			console.log(res.body);
+			res.should.have.status(200);
+			res.error.should.be.false;
+			res.body.should.be.an('string');
+			done();
+		});
+	});
 });
